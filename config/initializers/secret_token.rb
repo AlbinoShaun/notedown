2b35055d8720c6ad6eb9c3951eb2b8425b0d35ec
Shaun Whitely <albinoshaun@hotmail.com>
2013-05-18 15:04:16 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notedown::Application.config.secret_key_base = 'fb22fab4ff0bba238c93e9a1a1c47c2f0e240bece27d02077cc731f3b8e0c9d2d2482efc16b31e90e68ca24051b3c1eb1f540319674bbd13df833d263b1d8696'
