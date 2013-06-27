f8b55c50e0d1a6c71a8340011bc173c04350dc0c
Shaun Whitely <albinoshaun@hotmail.com>
2013-06-27 16:00:02 +1000

    added ability to update and delete own account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:index, :show, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update, :destroy]
 
 	def index
 	end
@@ -27,13 +28,29 @@ class UsersController < ApplicationController
 	end
 
 	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = 'Account updated!'
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		sign_out
+		@user.destroy
+		flash[:success] = 'Account deleted!'
+		redirect_to root_url
 	end
 
 	private
 		def user_params
 			params.require(:user).permit(:name, :password, :password_confirmation)
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to root_url unless current_user?(@user)
 		end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -37,4 +37,57 @@ describe "Users" do
 			end
 		end
 	end
+
+	describe "editing own user account" do
+		subject { page }
+
+		before do
+			@user = User.create name: 'Foo Bar', password: 'foobar', password_confirmation: 'foobar'
+			visit new_session_path
+			fill_in 'Name', with: @user.name
+			fill_in 'Password', with: @user.password
+			click_button 'Sign in'
+
+			visit edit_user_path(@user)
+		end
+
+		it { should have_field 'Name' }
+		it { should have_field 'Password' }
+		it { should have_button 'Save' }
+
+		describe 'updating details' do
+			before do
+				fill_in 'Name', with: 'Lorem Ipsum'
+				fill_in 'Password', with: 'loremipsum'
+				fill_in 'Confirm Password', with: 'loremipsum'
+				click_button 'Save'
+			end
+
+			it { should have_selector '.alert-success' }
+		end
+	end
+
+	describe "deleting own user account" do
+		before do
+			@user = User.create name: 'Foo Bar', password: 'foobar', password_confirmation: 'foobar'
+			visit new_session_path
+			fill_in 'Name', with: @user.name
+			fill_in 'Password', with: @user.password
+			click_button 'Sign in'
+			visit edit_user_path(@user)
+		end
+
+		it "should have a link to delete the user's account" do
+			page.should have_link 'Delete account'
+		end
+
+		it "should delete the account" do
+			expect { click_link 'Delete account' }.to change(User, :count).by(-1)
+		end
+
+		it "should sign the user out" do
+			click_link 'Delete account'
+			page.should have_link 'Sign in'
+		end
+	end
 end
