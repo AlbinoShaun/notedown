e48318c13e48b659c3dd35f2be40b80f1aa15f15
Shaun Whitely <albinoshaun@hotmail.com>
2013-06-27 14:22:49 +1000

    added sign in form

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -12,12 +12,32 @@ describe "Sessions" do
 			before { visit user_path(@user) }
 
 			it { should have_content 'Please sign in' }
+	
+			describe "signing in" do
+				before do
+					visit new_session_path
+					fill_in 'Name', with: @user.name
+					fill_in 'Password', with: @user.password
+					click_button 'Sign in'
+				end
+
+				it { should have_content 'Sign out' }
+				it { should have_content @user.name }
+			end
 		end
 	end
 
 	describe "when signed in" do
+		before do
+			visit new_session_path
+			fill_in 'Name', with: @user.name
+			fill_in 'Password', with: @user.password
+			click_button 'Sign in'
+		end
+
 		describe "visiting own page" do
-			
+			before { visit user_path(@user) }
+			it { should have_content 'Foo Bar' }
 		end
 	end
 end
