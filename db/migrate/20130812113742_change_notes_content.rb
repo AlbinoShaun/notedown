d9ec4905a3f3639af35bbdba2ecad0ce6e16dfbf
Shaun Whitely <albinoshaun@hotmail.com>
2013-08-12 21:47:38 +1000

    changed notes content to type text

---
+++ db/migrate/20130812113742_change_notes_content.rb
@@ -0,0 +1,9 @@
+class ChangeNotesContent < ActiveRecord::Migration
+  def up
+    change_column :notes, :content, :text
+  end
+  
+  def down
+    change_column :notes, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130629073142) do
+ActiveRecord::Schema.define(version: 20130812113742) do
 
   create_table "notebooks", force: true do |t|
     t.string   "title"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20130629073142) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content",     limit: 255
     t.string   "notebook_id"
     t.datetime "created_at"
     t.datetime "updated_at"
